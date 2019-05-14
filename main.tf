567df125d42ef0c12a8d30429feb0014ed0ab0fe
Ciaran Evans <aciaran@engineering.ukho.gov.uk>
2019-05-14 19:03:04 +0100

    Add oxalide example (That works)

--- main.tf
+++ main.tf
@@ -1,85 +1,192 @@
 provider "aws" {
-      region     = "${var.region}"
+  region     = "${var.region}"
 }
 
-resource "aws_ecs_cluster" "ciarans-cluster" {
-  name = "${module.label.name}"
-}
+data "aws_availability_zones" "available" {}
 
-module "label" {
-  source    = "git::https://github.com/cloudposse/terraform-terraform-label?ref=tags/0.11.0"
-  namespace = "ciaran"
-  stage     = "dev"
-  name      = "container"
-}
-
-module "vpc" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.4.1"
-  namespace  = "${module.label.namespace}"
-  stage      = "${module.label.stage}"
-  name       = "${module.label.name}"
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-module "dynamic_subnets" {
-  source             = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=master"
-  namespace  = "${module.label.namespace}"
-  stage      = "${module.label.stage}"
-  name       = "${module.label.name}"
-  region             = "${var.region}"
-  availability_zones = ["${var.region}a","${var.region}b","${var.region}c"]
-  vpc_id             = "${module.vpc.vpc_id}"
-  igw_id             = "${module.vpc.igw_id}"
-  cidr_block         = "10.0.0.0/16"
+resource "aws_subnet" "private" {
+  count             = "${var.az_count}"
+  cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  vpc_id            = "${aws_vpc.main.id}"
 }
 
+resource "aws_subnet" "public" {
+  count                   = "${var.az_count}"
+  cidr_block              = "${cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)}"
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  vpc_id                  = "${aws_vpc.main.id}"
+  map_public_ip_on_launch = true
+}
 
-module "container_definition" {
-  source          = "git::https://github.com/cloudposse/terraform-aws-ecs-container-definition?ref=tags/0.11.0"
-  container_name  = "${module.label.name}"
-  container_image = "ciaranevans/aws-parameters-spike:latest"
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+}
 
-  environment = [
-    {
-      name  = "ACTIVE_PROFILE"
-      value = "aws"
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.main.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.gw.id}"
+}
+
+resource "aws_eip" "gw" {
+  count      = "${var.az_count}"
+  vpc        = true
+  depends_on = ["aws_internet_gateway.gw"]
+}
+
+resource "aws_nat_gateway" "gw" {
+  count         = "${var.az_count}"
+  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
+  allocation_id = "${element(aws_eip.gw.*.id, count.index)}"
+}
+
+resource "aws_route_table" "private" {
+  count  = "${var.az_count}"
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${element(aws_nat_gateway.gw.*.id, count.index)}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = "${var.az_count}"
+  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+}
+
+resource "aws_security_group" "lb" {
+  name        = "tf-ecs-alb"
+  description = "controls access to the ALB"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["62.172.108.0/24"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ecs_tasks" {
+  name        = "tf-ecs-tasks"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = "${var.app_port}"
+    to_port         = "${var.app_port}"
+    security_groups = ["${aws_security_group.lb.id}"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_alb" "main" {
+  name            = "tf-ecs-chat"
+  subnets         = ["${aws_subnet.public.*.id}"]
+  security_groups = ["${aws_security_group.lb.id}"]
+}
+
+resource "aws_alb_target_group" "app" {
+  name        = "tf-ecs-chat"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = "${aws_vpc.main.id}"
+  target_type = "ip"
+}
+
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = "${aws_alb.main.id}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.app.id}"
+    type             = "forward"
+  }
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "aws-parameter-spike-cluster"
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "${var.fargate_cpu}"
+  memory                   = "${var.fargate_memory}"
+  execution_role_arn = "arn:aws:iam::748740125416:role/ecsTaskExecutionRole"
+
+  container_definitions = <<DEFINITION
+[
+  {
+    "cpu": ${var.fargate_cpu},
+    "image": "${var.app_image}",
+    "memory": ${var.fargate_memory},
+    "name": "app",
+    "networkMode": "awsvpc",
+    "portMappings": [
+      {
+        "containerPort": ${var.app_port},
+        "hostPort": ${var.app_port}
+      }
+    ],
+    "environment" : [
+      { "name" : "ACTIVE_PROFILE", "value" : "dev" }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group" : "awslogs-ciarans-app",
+        "awslogs-region" : "${var.region}",
+        "awslogs-stream-prefix" : "awslogs"
+      }
     }
-  ]
-
-  port_mappings = [
-    {
-      containerPort = 80
-      hostPort      = 80
-      protocol      = "tcp"
-    }
-  ]
+  }
+]
+DEFINITION
 }
 
-module "alb" {
-  source             = "git::https://github.com/cloudposse/terraform-aws-alb?ref=tags/0.2.6"
-  namespace          = "${module.label.namespace}"
-  name               = "${module.label.name}"
-  stage              = "${module.label.stage}"
+resource "aws_ecs_service" "main" {
+  name            = "tf-ecs-service"
+  cluster         = "${aws_ecs_cluster.main.id}"
+  task_definition = "${aws_ecs_task_definition.app.arn}"
+  desired_count   = "${var.app_count}"
+  launch_type     = "FARGATE"
 
-  vpc_id             = "${module.vpc.vpc_id}"
-  ip_address_type    = "ipv4"
+  network_configuration {
+    security_groups = ["${aws_security_group.ecs_tasks.id}"]
+    subnets         = ["${aws_subnet.private.*.id}"]
+  }
 
-  subnet_ids         = ["${module.dynamic_subnets.public_subnet_ids}"]
-  access_logs_region = "${var.region}"
-}
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.app.id}"
+    container_name   = "app"
+    container_port   = "${var.app_port}"
+  }
 
-module "alb_service_task" {
-  source                    = "git::https://github.com/cloudposse/terraform-aws-ecs-alb-service-task?ref=tags/0.11.0"
-  namespace                 = "${module.label.namespace}"
-  stage                     = "${module.label.stage}"
-  name                      = "${module.label.name}"
-  alb_target_group_arn      = "${module.alb.alb_arn}"
-  container_definition_json = "${module.container_definition.json}"
-  container_name            = "${module.label.name}"
-  ecs_cluster_arn           = "${aws_ecs_cluster.ciarans-cluster.arn}"
-  ignore_changes_task_definition = "false"
-  launch_type               = "FARGATE"
-  vpc_id                    = "${module.vpc.vpc_id}"
-  security_group_ids        = ["${module.alb.security_group_id}"]
-  subnet_ids        = ["${module.dynamic_subnets.public_subnet_ids}"]
+  depends_on = [
+    "aws_alb_listener.front_end",
+  ]
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,27 @@ variable "region" {
     default = "eu-west-2"
 }
 
+variable "az_count" {
+    description = "Number of AZs to cover in a given AWS region"
+    default     = "2"
+}
+
+variable "app_port" {
+    default = 8080
+}
+
+variable "fargate_cpu" {
+    default = 256
+}
+
+variable "app_image" {
+    default = "ciaranevans/aws-parameters-spike:latest"
+}
+
+variable "fargate_memory" {
+    default = "512"
+}
+
+variable "app_count" {
+    default = 1
+}
